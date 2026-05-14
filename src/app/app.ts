5ddc9ad12d76c0f6e50f7ad962e5bbc86187ed9d
Gustavo <gustavo.pereira@discente.ufg>
2026-05-14 08:57:59 -0300

    feat: add hamburger icon to menu button and fix sidenav background stripe

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 import { SidenavComponent } from './shared/components/sidenav/sidenav.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatSidenavModule, MatButtonModule, SidenavComponent],
+  imports: [RouterOutlet, MatSidenavModule, MatButtonModule, MatIconModule, SidenavComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
